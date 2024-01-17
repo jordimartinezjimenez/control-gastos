afcb1a71d8118471f59cb96f7dbbc574d32368f7
JORDI\Jordi <jordi_martinezjimenez@iescarlesvallbona.cat>
2024-01-17 11:29:02 +0100

    Added form and validation of the inserted budget

---
+++ src/components/ControlPresupuesto.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ControlPresupuesto = () => {
+    return (
+        <div className='contenedor contenedor-presupuesto sombra dos-columnas'>
+            
+        </div>
+    )
+}
+
+export default ControlPresupuesto
